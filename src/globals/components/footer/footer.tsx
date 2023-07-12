afe282ddc278e646c194bf495e96ae4a2d211a3b
Erys Mozo <mozoerys@gmail.com>
2023-07-13 00:27:30 +0800

    footer ui

--- src/globals/components/footer/footer.tsx
+++ src/globals/components/footer/footer.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
-
-type Props = {}
+type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
 
-export default Footer
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accesibility</p>
+        <p>This is not a real site</p>
+        <p>Houses</p>
+        <p>Referrals accepted</p>
+        <p>Fam</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>For the win</p>
+        <p>Airbnb luxury</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
