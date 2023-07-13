c72233331561de07d83d93882c7a80e9969d8199
Erys Mozo <mozoerys@gmail.com>
2023-07-13 23:05:39 +0800

    added react-date-range

--- src/globals/components/header/header.tsx
+++ src/globals/components/header/header.tsx
@@ -5,10 +5,39 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/solid";
 import Image from "next/image";
+import { useState } from "react";
+import { DateRangePicker } from "react-date-range";
+
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 
 type Props = {};
 
+type SelectionRange = {
+  selection: {
+    startDate: Date;
+    endDate: Date;
+  };
+};
+
 const Header = (props: Props) => {
+  const dateToday = new Date();
+
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(dateToday);
+  const [endDate, setEndDate] = useState(dateToday);
+
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges: SelectionRange | any) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   return (
     <header className="sticky top-0 bg-white z-50 grid grid-cols-3 shadow-md p-5 md:px-10">
       {/* LOGO */}
@@ -27,9 +56,11 @@ const Header = (props: Props) => {
         <input
           className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          name=""
-          id=""
+          name="search-input"
+          id="search-input"
           placeholder="Start your Search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 lg:cursor-pointer md:mx-2" />
       </div>
@@ -45,6 +76,17 @@ const Header = (props: Props) => {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+
+      {searchInput && (
+        <div>
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={dateToday}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+        </div>
+      )}
     </header>
   );
 };
