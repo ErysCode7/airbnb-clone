11cd34ed9592072a3e9e895f607d71f3aded14fd
Erys Mozo <mozoerys@gmail.com>
2023-07-15 07:32:45 +0800

    search page

---
+++ src/pages/search.tsx
@@ -0,0 +1,14 @@
+import { Search } from "@/modules/search";
+import type { NextPage } from "next";
+
+type Props = {};
+
+const SearchPage: NextPage<Props> = () => {
+  return (
+    <>
+      <Search />
+    </>
+  );
+};
+
+export default SearchPage;
